b24db134acd5d684872137ca3001869148cb3838
szrpf <27185709@qq.com>
2023-07-10 17:02:21 +0800

    v1.1

--- assets/Script/Helloworld.ts
+++ assets/Script/Helloworld.ts
@@ -5,10 +5,10 @@ const { ccclass, property } = cc._decorator;
 @ccclass
 export default class Helloworld extends cc.Component {
     progress: number = 0;
-    progressMax:number = 100;
+    progressMax: number = 100;
     lv: number = 0;
     protected start() {
-        this.node.getChildByName('进度+').on(cc.Node.EventType.TOUCH_START, () => {
+        cc.find('UI/进度+', this.node).on(cc.Node.EventType.TOUCH_START, () => {
             this.progress += 35;
             if (this.progress >= this.progressMax) {
                 this.progress -= this.progressMax;
@@ -16,10 +16,10 @@ export default class Helloworld extends cc.Component {
                 cc.find('残影进度条/进度', this.node).getComponent(EffectBar).setData(this.progressMax);
                 cc.find('弹射进度条/进度', this.node).getComponent(EffectBar).setData(this.progressMax);
                 cc.find('缩放进度条/进度', this.node).getComponent(EffectBar).setData(this.progressMax, this.progress);
-                this.node.getChildByName('等级').getComponent(cc.Label).string = `等级：${++this.lv}`;
+                cc.find('UI/等级', this.node).getComponent(cc.Label).string = `等级：${++this.lv}`;
             }
         }, this);
-        this.node.getChildByName('进度-').on(cc.Node.EventType.TOUCH_START, () => {
+        cc.find('UI/进度-', this.node).on(cc.Node.EventType.TOUCH_START, () => {
             this.progress = Math.max(this.progress - 45, 0);
         }, this);
     }
